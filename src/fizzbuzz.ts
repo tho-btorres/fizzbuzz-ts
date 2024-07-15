0069208b9bc8f885973c35d8f13d8aeeaa17ac3f
Byron Torres <tho.btorres@iberia.es>
2024-07-15 14:46:10 +0200

    refactor: extract contants for literals

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,14 +1,17 @@
+const FIZZ = "fizz";
+const BUZZ = "buzz";
+
 export const convert = (number: number): string => {
-  if (isDividedByThree(number) && isDividedByFive(number)) return "fizzbuzz";
-  if (isDividedByThree(number)) return "fizz";
-  if (isDividedByFive(number)) return "buzz";
+  if (isDividedByThree(number) && isDividedByFive(number)) return FIZZ + BUZZ;
+  if (isDividedByThree(number)) return FIZZ;
+  if (isDividedByFive(number)) return BUZZ;
   return number.toString();
 };
 
 const isDividedByThree = (number: number): boolean => {
   return number % 3 === 0;
-}
+};
 
 const isDividedByFive = (number: number): boolean => {
   return number % 5 === 0;
-}
+};
